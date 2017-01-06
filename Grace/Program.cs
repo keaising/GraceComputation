fc5a9a586926b7ae80b87ec0be869ee0a6104f28
Shuxiao Wang <keaising@gmail.com>
2017-01-06 10:01:16 +0800

    Add project files.

---
+++ Grace/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
