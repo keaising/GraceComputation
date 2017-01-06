b2d6f1c5b04830b65460968651f38b910a349ac7
Shuxiao Wang <keaising@gmail.com>
2017-01-06 10:21:25 +0800

    init

---
+++ Grace/Models/City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grace.Models
+{
+    public class City
+    {
+        public Int32 No_ { get; set; }
+        public String Name { get; set; }
+        public List<Int32> RelationCities { get; set; }
+        public Dictionary<Int32, Double> Distances { get; set; }
+    }
+}
--- Grace/Program.cs
+++ Grace/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
+
 
 namespace Grace
 {
@@ -10,6 +14,20 @@ namespace Grace
     {
         static void Main(string[] args)
         {
+            var grace = "Grace!";
+            WriteLine($"Hello {nameof(grace)}");
+            Mat();
+            ReadKey();
+        }
+
+        public static void Mat()
+        {
+            Matrix<double> A = DenseMatrix.OfArray(new double[,] {
+                                                    {1,1,1,1},
+                                                    {1,2,3,4},
+                                                    {4,3,2,1}});
+            Vector<double>[] nullspace = A.Kernel();
+            WriteLine(A.ToString());
         }
     }
 }
